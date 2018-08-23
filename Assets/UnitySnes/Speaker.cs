6e0a9eb73563e684567edbe2423fd8ca3ce9390f
buzzler <buzzler@unityscene.com>
2018-08-24 01:07:25 +0900

    Refactoring

--- Assets/UnitySnes/Speaker.cs
+++ Assets/UnitySnes/Speaker.cs
@@ -3,23 +3,21 @@ using UnityEngine;
 
 namespace UnitySnes
 {
-    [RequireComponent(typeof(AudioSource))]
     public class Speaker : MonoBehaviour
     {
-        private AudioSource _speaker;
+        public AudioSource AudioSource;
         private float[] _newData = new float[System.AudioBatchSize];
 
         private void Start()
         {
-            _speaker = GetComponent<AudioSource>();
-            _speaker.clip = AudioClip.Create("UnitySnes", System.AudioBatchSize / 2, 2, 32040, true, OnAudioRead); //44100
-            _speaker.loop = true;
+            AudioSource = GetComponent<AudioSource>();
+            AudioSource.clip = AudioClip.Create("UnitySnes", System.AudioBatchSize / 2, 2, 32040, true, OnAudioRead); //44100
         }
 
         public void UpdateAudio(float[] sampleData)
         {
             _newData = sampleData;
-            _speaker.Play();
+            AudioSource.Play();
         }
 
         private void OnAudioRead(float[] sampleData)
