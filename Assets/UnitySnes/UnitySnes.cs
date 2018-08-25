aa4829944638b40c44a7b6d518c916436bc5d7ee
buzzler <buzzler@unityscene.com>
2018-08-25 11:58:09 +0900

    Refactoring

--- Assets/UnitySnes/UnitySnes.cs
+++ Assets/UnitySnes/UnitySnes.cs
@@ -9,40 +9,34 @@ namespace UnitySnes
         public Renderer Display;
         public AudioSource AudioSource;
         public Texture2D Texture;
-        
         private System _system;
-        private const int AudioBufferSize = 4096;
-        private float[] _audioBuffer;
         
         private void Start()
         {
             Application.targetFrameRate = 60;
-            _audioBuffer = new float[AudioBufferSize];
-            AudioSource.clip = AudioClip.Create("UnitySnes", AudioBufferSize/2, 2, 44100, true, OnAudioRead);
-            AudioSource.loop = true;
-            AudioSource.Stop();
-
             TurnOn();
         }
 
         private void Update()
         {
             OnInputUpdate();
-            if (_system != null)
+            
+            // TODO : multi-thread...
+            if (IsTurnedOn)
                 _system.Update();
         }
 
         private void OnAudioUpdate()
         {
-            _audioBuffer = System.Buffers.AudioBuffer;
-            AudioSource.Play();
+            if (System.Buffers.AudioBuffer != null)
+                AudioSource.Play();
         }
-        
+
         private void OnAudioRead(float[] sampleData)
         {
-            Array.Copy(_audioBuffer, sampleData, sampleData.Length);
+            Array.Copy(System.Buffers.AudioBuffer, sampleData, sampleData.Length);
         }
-        
+
         private void OnVideoUpdate()
         {
             Texture.LoadRawTextureData(System.Buffers.VideoBuffer);
@@ -57,10 +51,10 @@ namespace UnitySnes
             inputBuffer[1] = (short) (Input.GetKey(KeyCode.A) || Input.GetButton("Y") ? 1 : 0);
             inputBuffer[2] = (short) (Input.GetKey(KeyCode.Space) || Input.GetButton("SELECT") ? 1 : 0);
             inputBuffer[3] = (short) (Input.GetKey(KeyCode.Return) || Input.GetButton("START") ? 1 : 0);
-            inputBuffer[4] = (short) (Input.GetKey(KeyCode.UpArrow) || Input.GetAxisRaw("DpadX")>=1f ? 1 : 0);
-            inputBuffer[5] = (short) (Input.GetKey(KeyCode.DownArrow) || Input.GetAxisRaw("DpadX")<=-1f ? 1 : 0);
-            inputBuffer[6] = (short) (Input.GetKey(KeyCode.LeftArrow) || Input.GetAxisRaw("DpadY")<=-1f ? 1 : 0);
-            inputBuffer[7] = (short) (Input.GetKey(KeyCode.RightArrow) || Input.GetAxisRaw("DpadY")>=1f ? 1 : 0);
+            inputBuffer[4] = (short) (Input.GetKey(KeyCode.UpArrow) || Input.GetAxisRaw("DpadX") >= 1f ? 1 : 0);
+            inputBuffer[5] = (short) (Input.GetKey(KeyCode.DownArrow) || Input.GetAxisRaw("DpadX") <= -1f ? 1 : 0);
+            inputBuffer[6] = (short) (Input.GetKey(KeyCode.LeftArrow) || Input.GetAxisRaw("DpadY") <= -1f ? 1 : 0);
+            inputBuffer[7] = (short) (Input.GetKey(KeyCode.RightArrow) || Input.GetAxisRaw("DpadY") >= 1f ? 1 : 0);
             inputBuffer[8] = (short) (Input.GetKey(KeyCode.X) || Input.GetButton("A") ? 1 : 0);
             inputBuffer[9] = (short) (Input.GetKey(KeyCode.S) || Input.GetButton("X") ? 1 : 0);
             inputBuffer[10] = (short) (Input.GetKey(KeyCode.Q) || Input.GetButton("L") ? 1 : 0);
@@ -76,15 +70,19 @@ namespace UnitySnes
         {
             if (_system != null)
                 return;
-            
+
             _system = new System();
             _system.Init(OnVideoUpdate, OnAudioUpdate);
             _system.LoadGame(Rom.bytes);
-            
+
             var w = Convert.ToInt32(System.SystemAvInfo.geometry.base_width);
             var h = Convert.ToInt32(System.SystemAvInfo.geometry.base_height);
             Texture = new Texture2D(w, h, TextureFormat.RGB565, false) {filterMode = FilterMode.Point};
             Display.material.mainTexture = Texture;
+            AudioSource.clip = AudioClip.Create(name, System.Buffers.AudioBufferSize / 2, 2, 44100, true, OnAudioRead);
+            AudioSource.playOnAwake = false;
+            AudioSource.spatialBlend = 0;
+            AudioSource.loop = true;
         }
 
         private void TurnOff()
@@ -92,16 +90,24 @@ namespace UnitySnes
             if (_system == null)
                 return;
 
+            AudioSource.Stop();
+            AudioSource.clip = null;
             Display.material.mainTexture = null;
             Texture = null;
+
             _system.UnloadGame();
             _system.DeInit();
             _system = null;
         }
-        
+
         private void OnDisable()
         {
             TurnOff();
+        }
+        
+        public bool IsTurnedOn
+        {
+            get { return _system != null; }
         }
     }
 }
