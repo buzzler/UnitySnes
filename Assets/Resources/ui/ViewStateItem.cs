38185b6c634fef870a2294dcf188aed659f93a5f
buzzler <buzzler@unityscene.com>
2018-11-29 00:43:18 +0900

    누락 파일 추가

---
+++ Assets/Resources/ui/ViewStateItem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnitySnes
+{
+    public class ViewStateItem : MonoBehaviour
+    {
+        public Button LoadButton;
+        public Button DeleteButton;
+        public Text Label;
+    }
+}
